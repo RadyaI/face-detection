91e38ef98a136a1158a09dbd076cbc305b1f553c
RadyaI <radyaiftikhar@gmail.com>
2024-08-23 14:00:29 +0700

    update : main file

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import threading
 pygame.mixer.init()
 
 # Load the sound
-sound = pygame.mixer.Sound('welcome.mp3')
+sound = pygame.mixer.Sound('OpenGate.mp3')
 
 repeat_sound = False
 
 def play_sound():
+    print("Playing sound")
     sound.play()
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-video_capture = cv2.VideoCapture(1) 
+video_capture = cv2.VideoCapture(1) # ganti jadi (0) kalo pake kamera default
 
 face_detected_last_time = False  
 sound_played = False  
@@ -30,23 +31,23 @@ while True:
 
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
     if len(faces) > 0:
         if repeat_sound:
             if not face_detected_last_time:
                 threading.Thread(target=play_sound).start()
+                print("Face detected - playing sound")
                 face_detected_last_time = True
         else:
             if not sound_played:
                 threading.Thread(target=play_sound).start()
+                print("Face detected - playing sound")
                 sound_played = True
             face_detected_last_time = True
     else:
         face_detected_last_time = False
 
-    cv2.imshow('Video', frame)
+    # Ini biar ga muncul popup videonya
+    # cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
